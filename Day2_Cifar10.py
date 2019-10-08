21e2bb29e46dce1b2e8a01fdceb9f941c8a409de
INCIBMB <50316838+INCIBMB@users.noreply.github.com>
2019-10-08 15:40:37 +0300

    Add files via upload

---
+++ Day2_Cifar10.py
@@ -0,0 +1,33 @@
+from keras.datasets import cifar10
+from keras.utils import np_utils
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.layers.convolutional import Conv2D, MaxPooling2D
+from keras.optimizers import SGD, Adam, RMSprop
+import matplotlib.pyplot as plt
+# CIFAR_10 is a set of 60K images 32x32 pixels on 3 channels
+IMG_CHANNELS = 3
+IMG_ROWS = 32
+IMG_COLS = 32
+#constant
+BATCH_SIZE = 128
+NB_EPOCH = 20
+NB_CLASSES = 10
+VERBOSE = 1
+VALIDATION_SPLIT = 0.2
+OPTIM = RMSprop()
+
+#load dataset
+(X_train, y_train), (X_test, y_test) = cifar10.load_data()
+print('X_train shape:', X_train.shape)
+print(X_train.shape[0], 'train samples')
+print(X_test.shape[0], 'test samples')
+
+# convert to categorical
+Y_train = np_utils.to_categorical(y_train, NB_CLASSES)
+Y_test = np_utils.to_categorical(y_test, NB_CLASSES)
+# float and normalization
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+X_train /= 255
+X_test /= 255
---
+++ Day2_Mnist.py
@@ -0,0 +1,13 @@
+from keras import backend as k
+from keras.models import Sequential
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPooling2D
+from keras.layers.core import Activation
+from keras.layers.core import Flatten
+from keras.layers.core import Dense
+from keras.datasets import mnist
+from keras.utils import np_utils
+from keras.optimizers import SGD, RMSprop, Adam
+import numpy as np
+import matplotlib.pyplot as plt
+
